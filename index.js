657dd4b0d065181e4271c1b405bdb0f4586b941e
koinoniays2 <koinonia_ys@naver.com>
2024-02-18 04:00:53 +0900

    프로젝트제목표시

--- index.js
+++ index.js
@@ -16,3 +16,34 @@ $(document).ready(function () {
         nextArrow: $('.next-button'),
     });
 });
+
+window.addEventListener("load", () => {
+    window.addEventListener('scroll', scrollGuild);
+})
+function scrollGuild() {
+    const header = document.querySelector("#header");
+    const projectGuild = document.querySelector("#project .project-1");
+    const titleGuild = document.querySelector("#project .title > p:nth-child(2)");
+    const projectTest = document.querySelector("#project .project-2");
+    const test = document.querySelector("#project .title > p:nth-child(3)")
+
+    const headerBottom = header.getBoundingClientRect().bottom;
+    if (projectGuild.getBoundingClientRect().top <= headerBottom) {
+        titleGuild.style.opacity = "1";
+    } else {
+        titleGuild.style.opacity = "0";
+    }
+
+    if (projectTest.getBoundingClientRect().top <= headerBottom) {
+        titleGuild.style.opacity = "0";
+        titleGuild.style.display = "none";
+        test.style.display = "block";
+        setTimeout(() => {
+            test.style.opacity = "1";
+        }, 300);
+    } else {
+        test.style.opacity = "0";
+        test.style.display = "none";
+        titleGuild.style.display = "block";
+    }
+}
